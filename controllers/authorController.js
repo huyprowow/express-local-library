5dc16e80ce4b8c30c24328b67865af90ebf169e3
Huypro <huyprowow@gmail.com>
2021-10-20 20:35:52 +0700

    trang chi tiet tac gia

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,20 +1,51 @@
 var Author = require("../models/author");
+var async = require("async");
+var Book = require("../models/book");
 
 //ht danh sach tat ca cac tac gia
 exports.author_list = function (req, res) {
   //xu li xong req =>k co ham next
   // res.send("NOT IMPLEMENT : Author list");
   Author.find()
-  .sort([['family_name','ascending']])
-  .exec(function (err,list_author){
-    if (err) {return next(err);}
-    res.render('author_list',{title:'Author List',author_list:list_author})
-  })
+    .sort([["family_name", "ascending"]])
+    .exec(function (err, list_author) {
+      if (err) {
+        return next(err);
+      }
+      res.render("author_list", {
+        title: "Author List",
+        author_list: list_author,
+      });
+    });
 };
 
 //ht trang chi tiet cho 1 tac gia cu the
 exports.author_detail = function (req, res) {
-  res.send("NOT IMPLEMENT : Author detail " + req.params.id);
+  // res.send("NOT IMPLEMENT : Author detail " + req.params.id);
+
+  async.parallel(
+    {
+      author: function (callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books: function (callback) {
+        Book.find({ author: req.params.id }, "title summary").exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) return next(err);
+      if (results.author === null) {
+        var err = new Error("Author not found");
+        err.status = 404;
+        return next(err);
+      }
+      res.render("author_detail", {
+        title: "Author Detail",
+        author: results.author,
+        author_books: results.authors_books,
+      });
+    }
+  );
 };
 
 //ht tao bieu mau tac gia tren GET
